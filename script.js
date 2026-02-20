b33e8b40480054f1fd7c256c392719771159620e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-20 11:05:04 +0600

    fetche test

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hi");
+
+// const fetchAllLevel = async (id) => {
+//     try {
+//         const res = await fetch(`https://openapi.programming-hero.com/api/words/${id}`);
+//         const data = await res.json();
+//         // displayShow(data);
+//         console.log(data.data);
+
+//     } catch (error) {
+//         console.log("Error Message", error);
+//     }
+// }
+// fetchAllLevel(1)
+
+
+// const fetchAllLevel = async (id) => {
+//     try {
+//         const res = await fetch(`https://openapi.programming-hero.com/api/word/${id}`);
+//         const data = await res.json();
+
+      
+//         console.log("Words:", data.data);
+
+//     } catch (error) {
+//         console.log("Error Message", error);
+//     }
+// }
+
+// fetchAllLevel(5);
+
+// All api fetching successfully;
+
+
+
