b489e12d9c3ba30b2793d8996d89a9631d14d991
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-20 14:27:53 +0600

    Submit form valdation

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 //         const res = await fetch(`https://openapi.programming-hero.com/api/word/${id}`);
 //         const data = await res.json();
 
-      
+
 //         console.log("Words:", data.data);
 
 //     } catch (error) {
@@ -31,4 +31,34 @@
 // All api fetching successfully;
 
 
+// Globaly declar name + email;
+const yourName = "Md.Asik"
+const yourEmail = "mdasik855789@gmail.com"
+// Form submit code start here;
+let subMitBtn = document.getElementById("subMitBtn");
+subMitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const name = getInputFieldValue("name")
+    const email = getInputFieldValue("email")
+    // console.log(name,email);
+    // name validation code;
+    if(name !==yourName){
+        alert("Name not match")
+        return;
+    }
+    // email validation code here;
+    if(email !== yourEmail){
+        // console.log("success");
+        alert("please provide right email")
+    }else{
+        alert("Submit Successfully")
+    }
 
+
+})
+// Form submit code end here;
+
+// Reusable funk code;
+const getInputFieldValue = (id) => {
+    return document.getElementById(id).value
+}
